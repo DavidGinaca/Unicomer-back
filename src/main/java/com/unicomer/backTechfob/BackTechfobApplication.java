6890d98c2d467b5b431451f0de97ae8ee85bfb34
David <davidginaca@gmail.com>
2023-12-17 10:35:02 -0300

    primer comentario

---
+++ src/main/java/com/unicomer/backTechfob/BackTechfobApplication.java
@@ -0,0 +1,13 @@
+package com.unicomer.backTechfob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTechfobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTechfobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicomer/backTechfob/BackTechfobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicomer.backTechfob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTechfobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
